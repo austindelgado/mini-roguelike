1e351aa9f01d8ca4afbcd9264d63e78518b41c50
Austin <austinrdelgado@gmail.com>
2022-02-05 20:01:02 -0500

    Added enemy spawner and spawn points

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform[] spawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = transform.childCount;
+        spawnPoints = new Transform[count];
+        for(int i = 0; i < count; i++){
+            spawnPoints[i] = transform.GetChild(i);
+        }
+
+        SpawnEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, transform.rotation);
+    }
+}
