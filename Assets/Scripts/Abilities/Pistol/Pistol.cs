a8e502c8b9f837ff8eefe1a0439a9e2dc78e6a3a
Austin <austinrdelgado@gmail.com>
2022-02-07 16:29:33 -0500

    Added Pistol ability

---
+++ Assets/Scripts/Abilities/Pistol/Pistol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Pistol : Ability
+{
+    public GameObject projectilePrefab;
+
+    public override void Activate(GameObject parent)
+    {
+        GameObject projectile = Instantiate(projectilePrefab, parent.transform.position, parent.transform.rotation);
+        projectile.GetComponent<Projectile>().dir = parent.GetComponent<CharacterController>().lookDir;
+    }
+}
