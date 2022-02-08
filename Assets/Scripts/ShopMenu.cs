b7a3207d6c972ebbef5b3441ccc8f5dab20b9bb4
Austin <austinrdelgado@gmail.com>
2022-02-08 17:22:50 -0500

    Added Shop Menu with ability select buttons

---
+++ Assets/Scripts/ShopMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopMenu : MonoBehaviour
+{
+    public GameObject shopUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void AbilitySelect(int num)
+    {
+
+    }
+
+    public void Play()
+    {
+        shopUI.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+}
