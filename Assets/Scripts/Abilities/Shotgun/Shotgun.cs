c7d89ae0d8a9821a03b8bafdbb7ed087b25ada15
Austin <austinrdelgado@gmail.com>
2022-02-08 17:07:46 -0500

    Shotgun added

---
+++ Assets/Scripts/Abilities/Shotgun/Shotgun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Shotgun : Ability
+{
+    public GameObject projectilePrefab;
+
+    public float maxSpread;
+    public int[] pellets;
+
+    public override void Activate(GameObject parent, int level)
+    {
+        Vector2 shotDir = parent.GetComponent<CharacterController>().lookDir;
+        for (int i = 0; i < pellets[level]; i++)
+        {
+            float shotAngle = Random.Range(-maxSpread, maxSpread);
+            GameObject projectile = Instantiate(projectilePrefab, parent.transform.position, parent.transform.rotation);
+            projectile.GetComponent<Projectile>().dir = new Vector2(shotDir.x * Mathf.Cos(shotAngle * Mathf.Deg2Rad) - shotDir.y * Mathf.Sin(shotAngle * Mathf.Deg2Rad), shotDir.x * Mathf.Sin(shotAngle * Mathf.Deg2Rad) + shotDir.y * Mathf.Cos(shotAngle * Mathf.Deg2Rad));
+        }
+    }
+}
