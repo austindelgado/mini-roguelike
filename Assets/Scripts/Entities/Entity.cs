52dab463c42346e2f8cabfcb4dd573fc79b9cde2
Austin <austinrdelgado@gmail.com>
2022-02-09 12:22:14 -0500

    Added health and Damage function to entity

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    public int startingHealth;
+    public int currentHealth;
+
     public float speed;
     public float acceleration;
     public float deceleration;
@@ -11,6 +14,14 @@ public class Entity : MonoBehaviour
     public Vector2 velocity;
     public Vector2 moveInput;
     public Vector2 lookDir;
+
+
+    public virtual void Damage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth < 0)
+            Kill();
+    }
 
     public virtual void Kill(){}
 }
