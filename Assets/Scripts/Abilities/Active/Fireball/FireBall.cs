d17022dc2c7722ce1faf95b315a73c788c9317ea
Austin <austinrdelgado@gmail.com>
2022-02-13 14:09:53 -0500

    Added Fireballa active and projectile

---
+++ Assets/Scripts/Abilities/Active/Fireball/FireBall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FireBall : Ability
+{
+    public GameObject projectilePrefab;
+    public int[] damage;
+    public float[] distance;
+    public float[] scaling;
+
+    public override void Activate(GameObject parent, int level)
+    {
+        GameObject projectile = Instantiate(projectilePrefab, parent.transform.position, parent.transform.rotation);
+        projectile.GetComponent<FireBallProjectile>().parent = parent;
+        projectile.GetComponent<FireBallProjectile>().dir = parent.GetComponent<Entity>().lookDir;
+        projectile.GetComponent<FireBallProjectile>().damage = damage[level];
+        projectile.GetComponent<FireBallProjectile>().maxDistance = distance[level];
+        projectile.GetComponent<FireBallProjectile>().scaling = scaling[level];
+    }
+}
---
+++ Assets/Scripts/Abilities/Active/Fireball/FireBallProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallProjectile : Projectile
+{
+    private Vector3 startingScale;
+
+    public float scaling;
+    
+    public override void Start()
+    {
+        base.Start();
+        startingScale = transform.localScale;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        Debug.Log(distance);
+        transform.localScale = new Vector3(startingScale.x + distance * scaling, startingScale.y + distance * scaling, startingScale.z);
+    }
+}
