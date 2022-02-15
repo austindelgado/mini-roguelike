c3708d1f35b543f302f6dea8b56cf48a4899ef3f
Austin <austinrdelgado@gmail.com>
2022-02-15 17:50:23 -0500

    Added AbilityUI and Icon

---
+++ Assets/AbilityIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityIcon : MonoBehaviour
+{
+    public AbilitySlot slot;
+    public Image image;
+
+    public void Update()
+    {
+
+    }
+
+    public void Setup(AbilitySlot slot)
+    {
+        this.slot = slot;
+
+        if (slot.ability != null)
+            image.sprite = slot.ability.icon;
+    }
+}
