2ea6db6741e544d48a4359e5a3a2e22d8cd5156d
Austin <austinrdelgado@gmail.com>
2022-02-19 17:32:57 -0500

    Updated MainMenu to support networking

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private NetworkManagerLobby networkManager = null;
+
+    [Header("UI")]
+    [SerializeField] private GameObject landingPagePanel = null;
+
+    public void HostLobby()
+    {
+        networkManager.StartHost();
+
+        landingPagePanel.SetActive(false); 
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
