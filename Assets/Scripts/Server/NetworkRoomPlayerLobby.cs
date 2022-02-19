675dc2326c002f61240ed080a065d86e8c87229c
Austin <austinrdelgado@gmail.com>
2022-02-19 18:29:38 -0500

    Added start game

--- Assets/Scripts/Server/NetworkRoomPlayerLobby.cs
+++ Assets/Scripts/Server/NetworkRoomPlayerLobby.cs
@@ -121,6 +121,6 @@ public class NetworkRoomPlayerLobby : NetworkBehaviour
         if (Room.RoomPlayers[0].connectionToClient != connectionToClient)
             return;
 
-        // Start Game
+        Room.StartGame();
     }
 }
