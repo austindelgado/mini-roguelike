b9459f3ad3e9e8db3c19251bedba571b88fac943
Austin <austinrdelgado@gmail.com>
2022-03-14 21:18:27 -0400

    Changed projectiles to be non networked

--- Assets/Scripts/Abilities/Projectile.cs
+++ Assets/Scripts/Abilities/Projectile.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
 
-public class Projectile : NetworkBehaviour
+public class Projectile : MonoBehaviour
 {
-    [SyncVar] public GameObject parent;
+    public GameObject parent;
 
     public Vector2 startingPos;
     public Vector2 dir;
@@ -16,6 +15,8 @@ public class Projectile : NetworkBehaviour
 
     public float distanceTraveled;
     public float maxDistance;
+
+    private float catchupDistance = 0f;
 
     public virtual void Start()
     {
@@ -28,37 +29,58 @@ public class Projectile : NetworkBehaviour
         distanceTraveled = distanceVector.magnitude;
 
         if (maxDistance != 0 && distanceTraveled > maxDistance)
-            NetworkServer.Destroy(gameObject);
+            Destroy(gameObject);
     }
 
     public virtual void FixedUpdate()
     {
-        // Happens on cluent and server
-        transform.Translate(Vector2.right * speed * Time.fixedDeltaTime);
+        // Happens on client and server
 
-        if(isServer)
+        float moveValue = speed * Time.deltaTime;
+        float catchupValue = 0f;
+
+        if (catchupDistance > 0f)
         {
-            // Collision happens only on server
-            Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, circleCollider.radius * transform.localScale.x);
-            foreach (Collider2D hit in hits)
+            float step = catchupDistance * Time.deltaTime;
+            catchupDistance -= step;
+
+            catchupValue = step;
+
+            if (catchupDistance < (moveValue * 0.1f))
             {
-                if (hit.gameObject == null || hit.gameObject == gameObject || hit.gameObject == parent)
-                    continue;
-
-                // Hitting something not a wall and not yourself
-                if (hit.gameObject.tag != "Wall" && hit.gameObject != parent)
-                {
-                    if(!hit.gameObject.TryGetComponent<Health>(out var damageable))
-                        continue;
-
-                    damageable.DealDamage(damage);
-
-                    if (!piercing)
-                        NetworkServer.Destroy(gameObject);
-                }
-                else
-                    NetworkServer.Destroy(gameObject);
+                catchupValue += catchupDistance;
+                catchupDistance = 0f;
             }
         }
+
+        transform.Translate(Vector2.right * (moveValue + catchupValue));
+
+        // Collision happens only on server
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, circleCollider.radius * transform.localScale.x);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.gameObject == null || hit.gameObject == gameObject || hit.gameObject == parent)
+                continue;
+
+            // Hitting something not a wall and not yourself
+            if (hit.gameObject.tag != "Wall" && hit.gameObject != parent)
+            {
+                if(!hit.gameObject.TryGetComponent<Health>(out var damageable))
+                    continue;
+
+                damageable.DealDamage(damage);
+
+                if (!piercing)
+                    Destroy(gameObject);
+            }
+            else
+                Destroy(gameObject);
+        }
+    }
+
+    public void Initialize(GameObject parent, float duration)
+    {
+        catchupDistance = duration * speed;
+        this.parent = parent;
     }
 }
