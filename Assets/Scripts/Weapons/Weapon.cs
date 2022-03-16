798d2c6845aa039a65e03538d58baa58254ca3a6
Austin <austinrdelgado@gmail.com>
2022-03-16 18:54:37 -0400

    Setup new Weapon Class

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Weapon : NetworkBehaviour
+{
+    WeaponData weaponData;
+    public int currentAmmo;
+
+    public Projectile projectilePrefab; // Here because all are shared for now
+
+    private void Fire()
+    {
+        if (!isServer)
+        {
+            Projectile projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+            projectile.Initialize(gameObject, 0f);
+        }
+
+        CmdFire(gameObject, transform.position, transform.rotation, NetworkTime.time);
+    }
+
+    [Command]
+    void CmdFire(GameObject parent, Vector3 position, Quaternion rotation, double networkTime)
+    {
+        double timePassed = NetworkTime.time - networkTime;
+
+        Projectile projectile = Instantiate(projectilePrefab, position, rotation);
+        projectile.Initialize(parent, (float)timePassed); // Add passing in parent here
+
+        RpcFire(parent, position, rotation, networkTime);
+    }
+
+    [ClientRpc]
+    void RpcFire(GameObject parent, Vector3 position, Quaternion rotation, double networkTime)
+    {
+        if (hasAuthority)
+            return;
+
+        double timePassed = NetworkTime.time - networkTime;
+
+        Projectile projectile = Instantiate(projectilePrefab, position, rotation);
+        projectile.Initialize(parent, (float)timePassed); // Add passing in parent here
+    }
+}
